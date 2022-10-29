e766e3cedec8f9257dc08e973b6d2bcd8f568a0a
Nathan <nateberhe@aol.com>
2022-10-29 01:54:35 -0400

    Finished up the js sheet, and changed the color gradient on CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,38 +10,40 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('#flip').addEventListener('click', function() {
         if (Math.random() > .499999999) {
             //let's assume heads
-            document.querySelector("#penny-image").src = 'assets/images/penny-heads.jpg'
-            // TODO: Update image and status message in the DOM
-            document.querySelector('#message').innerHTML = "You Flipped Heads!"
+
             // Update the scorboard
             // TODO: Calculate the total number of rolls/flips
-            headsFlips++
-            totalFlips++
-            headsPercent = Math.round(headsFlips / totalFlips * 100)
-            tailsPercent = Math.round(tailsFlips / totalFlips * 100)
-            document.querySelector('#heads').innerHTML = headsFlips
-            document.querySelector('#heads-percent').innerHTML = headsPercent
-            document.querySelector('#tails').innerHTML = tailsFlips
-            document.querySelector('#tails-percent').innerHTML = tailsPercent
+            headsFlips++;
+            totalFlips++;
+            headsPercent = Math.round((headsFlips / totalFlips) * 100);
+            tailsPercent = Math.round((tailsFlips / totalFlips) * 100);
+            document.querySelector("#heads").innerHTML = headsFlips;
+            document.querySelector("#heads-percent").innerHTML = headsPercent + '%';
+            document.querySelector("#tails").innerHTML = tailsFlips;
+            document.querySelector("#tails-percent").innerHTML = tailsPercent + '%';
+            // TODO: Update image and status message in the DOM
+            document.querySelector("#penny-image").src = "assets/images/penny-heads.jpg";
+            document.querySelector("#message").innerHTML = "You Flipped Heads!";
             // Make variables to track the percentages of heads and tails
             // TODO: Use the calculated total to calculate the percentages
             // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
             // TODO: Update the display of each table cell
         } else {
             //let's assume tails
-            document.querySelector("#penny-image").src = "assets/images/penny-tails.jpg";
-            // TODO: Update image and status message in the DOM
-            document.querySelector("#message").innerHTML = "You Flipped Tails!";
+
             // Update the scorboard
             // TODO: Calculate the total number of rolls/flips
-            tailsFlips++
-            totalFlips++
-            headsPercent = Math.round(headsFlips / totalFlips * 100)
-            tailsPercent = Math.round(tailsFlips/totalFlips * 100)
+            tailsFlips++;
+            totalFlips++;
+            headsPercent = Math.round((headsFlips / totalFlips) * 100);
+            tailsPercent = Math.round((tailsFlips / totalFlips) * 100);
             document.querySelector("#heads").innerHTML = headsFlips;
-            document.querySelector("#heads-percent").innerHTML = headsPercent;
+            document.querySelector("#heads-percent").innerHTML = headsPercent + '%';
             document.querySelector("#tails").innerHTML = tailsFlips;
-            document.querySelector("#tails-percent").innerHTML = tailsPercent;
+            document.querySelector("#tails-percent").innerHTML = tailsPercent + '%';
+            // TODO: Update image and status message in the DOM
+            document.querySelector("#penny-image").src ="assets/images/penny-tails.jpg";
+            document.querySelector("#message").innerHTML = "You Flipped Tails!";
 
             // Make variables to track the percentages of heads and tails
             // TODO: Use the calculated total to calculate the percentages
@@ -56,13 +58,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Clear Button Click Handler
     document.querySelector('#clear').addEventListener('click', function () {
-        headsFlips = 0
-        headsPercent = '0%'
-        tailsFlips = 0
-        tailsPercent = '0%'
-        totalFlips = 0
+        headsFlips = 0;
+        headsPercent = "0%";
+        tailsFlips = 0;
+        tailsPercent = "0%";
+        totalFlips = 0;
+        document.querySelector("#heads").innerHTML = headsFlips;
+        document.querySelector("#heads-percent").innerHTML = headsPercent;
+        document.querySelector("#tails").innerHTML = tailsFlips;
+        document.querySelector("#tails-percent").innerHTML = tailsPercent;
+        // TODO: Update the scoreboard (same logic as in flip button click handler)
+        document.querySelector("#penny-image").src = "assets/images/penny-heads.jpg";
         document.querySelector("#message").innerHTML = "Let's Get Flipping!";
-      // TODO: Update the scoreboard (same logic as in flip button click handler)
     })
 
 
